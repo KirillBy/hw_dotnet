0643c04a116bd932d16e595e3d4a888f972294ec
Kiryl Balanovich <balanovichks@gmail.com>
2019-12-02 11:47:08 +0300

    second commit: image succesfully created

--- HW2/HW.02/Program.cs
+++ HW2/HW.02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HW._02
 {
@@ -6,6 +7,20 @@ namespace HW._02
     {
         static void Main(string[] args)
         {
+
+            StreamReader textReader = new StreamReader(@"C:\Users\Admin\Downloads\image.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+            textReader.Dispose();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+            for(int i = 0; i < arrayOfTextResult.Length-1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+                Console.WriteLine(arrayOfTextResult[i]);
+            }
+            File.WriteAllBytes(@"C:\Users\Admin\Downloads\image.png", imageBytes);
+            
             Console.WriteLine("Hello World!");
         }
     }
