5fbff24e8fb3f802cf72c67b3034f273297bde3c
Kiryl Balanovich <balanovichks@gmail.com>
2019-12-12 10:00:14 +0300

    hw 4 first task done

---
+++ ItAcademyHW/HW04.Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW04.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int lastCapLetter = 90;       //Last capital letter ASCII code
+            const int firstCapLetter = 65;      ////First capital letter ASCII code
+            for (int i = lastCapLetter; i >= firstCapLetter; i--)
+            {
+                char englCapLetter = (char)i;   //conver ASCII code to capital letter
+                char englLowLetter = (char)(i + 32); //conver ASCII code to low letter
+                Console.WriteLine(englCapLetter + " " + englLowLetter);
+            }
+            
+        }
+    }
+}
