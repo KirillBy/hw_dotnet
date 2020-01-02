e440333ba7d00a73e44b82c5f8b0ac2749450f23
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-02 12:40:43 +0300

    HW8 task 2 is done

---
+++ ItAcademyHW/AssemblyOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AssemblyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeInternal employeeInternal = new EmployeeInternal();
+            NewEmployeeInternal newEmployeeInternal = new NewEmployeeInternal();
+            EmployeePublic employeePublic = new EmployeePublic();
+            NewEmployeePublic newEmployeePublic = new NewEmployeePublic();
+            employeeInternal.salaryProtInternal = 1000; //this element avalible here as protected internal
+            employeeInternal.ChangeNamePublic("SomeName"); //this method avalible here as protected internal
+            employeePublic.ChangeNamePublic("SomeName"); //this method avalible here as protected internal
+            employeePublic.salaryProtInternal = 1000; //this element avalible here as protected internal
+            newEmployeeInternal.salaryProtInternal = 1000; //this element avalible here as protected internal
+            newEmployeeInternal.ChangeNamePublic("SomeName"); //this method avalible here as protected internal
+            newEmployeePublic.salaryProtInternal = 1000; //this element avalible here as protected internal
+            newEmployeePublic.ChangeNamePublic("SomeName"); //this method avalible here as protected internal
+        }
+    }
+}
---
+++ ItAcademyHW/AssemblyTwo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AssemblyOne;
+
+namespace AssemblyTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EmployeePublic employeePublic = new EmployeePublic();
+            NewEmployeePublic newEmployeePublic = new NewEmployeePublic();
+            employeePublic.ChangeNamePublic("SomeName"); //this method avalible here as protected internal
+            newEmployeePublic.ChangeNamePublic("SomeName"); //this method avalible here as protected internal
+        }
+    }
+}
