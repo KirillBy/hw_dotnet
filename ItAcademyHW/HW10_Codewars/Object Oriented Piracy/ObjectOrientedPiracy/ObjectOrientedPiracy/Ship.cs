b64cef2d066bb5a1c3a67ab41f052c999ea0a46b
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-15 14:23:15 +0300

    task 1 object oriented pirates

---
+++ ItAcademyHW/HW10_Codewars/Object Oriented Piracy/ObjectOrientedPiracy/ObjectOrientedPiracy/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedPiracy
+{
+    public class Ship
+    {
+        public int Draft;
+        public int Crew;
+
+        public Ship(int draft, int crew)
+        {
+            Draft = draft;
+            Crew = crew;
+        }
+
+        public bool IsWorthIt()
+        {
+            return Draft - (Crew * 1.5) > 20 ? true : false;
+        }
+    }
+}
