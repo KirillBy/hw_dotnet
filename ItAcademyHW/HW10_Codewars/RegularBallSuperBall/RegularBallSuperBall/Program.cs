1810643afe790a0bee303b5a56d5f73eeaa91fe6
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-15 15:52:33 +0300

    task 3 ball regular super ball

---
+++ ItAcademyHW/HW10_Codewars/RegularBallSuperBall/RegularBallSuperBall/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RegularBallSuperBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//Create a class Ball.
+
+//Ball objects should accept one argument for "ball type" when instantiated.
+
+//If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
