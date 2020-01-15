ccc58df7011632b1bef3ee9c1c37515a1cc2a19e
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-15 16:12:26 +0300

    Task 3 Bulding blocks

---
+++ ItAcademyHW/HW10_Codewars/BuildingBlocks/BuildingBlocks/Block.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks
+{
+    class Block
+    {
+        private int width;
+        private int length;
+        private int height;
+
+        public Block(int[] block)
+        {
+            width = block[0];
+            length = block[1];
+            height = block[2];
+        }
+
+        public int GetWidth() {return width;}
+        public int GetLength() { return length; }
+        public int GetHeight() { return height; }
+        public int GetVolume() { return width * length * height; }
+        public int GetSurfaceArea() { return 2 * (width*length + length*height
+                + width*height); }
+    }
+}
